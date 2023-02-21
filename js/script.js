cac819a00fb5ad8ba4c09cdcab8ab8e7622362c2
Junior Franca <121913487+juniorfranc4h@users.noreply.github.com>
2023-02-21 19:17:35 -0300

    Meu primeiro projeto

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let modal = document.querySelector('.modal')
+// let corpo = document.querySelector('body')
+
+function modalLogar() {            
+    modal.style.display = 'block'
+}
+
+function fecharModal() {
+    modal.style.display = 'none'
+}
